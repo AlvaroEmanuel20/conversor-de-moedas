cb2fc69d8344ef9ae4bc77ce5245e5a2beef13db
Álvaro Emanuel <alvarodiasribeiro16@gmail.com>
2022-08-26 00:58:16 -0300

    feat(refatoração): melhoramento do código
    Fiz uns ajustes para uma melhor legibilidade e sintaxe. Features:
    -Método para formatar moeda
    -Método para formatar data
    -Pequena alteração no style do conversor

--- README.md
+++ README.md
@@ -3,11 +3,6 @@
 
 Precisa comprar algo em uma moeda diferente e quer saber quanto custa em sua moeda? Ou vai viajar e precisa calcular certo quanto vai levar? Então use o conversor de moedas e tenha acesso a uma cotação atualizada a cada 30 segundos com base em dados da [Awesome API Cotação de moedas](https://docs.awesomeapi.com.br/api-de-moedas).
 
-![Print da home do projeto](./images/Screenshot_index.png)
-
-
-
-
 ## 📋 Funcionalidades
 
 Para utilizar o conversor basta inserir uma quantia no campo "Insira a quantia:" selecionar a moeda de partida e a final e por fim clicar em "Converter Quantia".
@@ -26,25 +21,14 @@ Além disso na parte de baixo da calculadora é possível ver a cotação atuali
 E por fim ao realizar a conversão o resultado traz a moeda formatada.
 ## ✅ Tecnologias utilizadas
 
-Para criação desse projeto identifiquei que utilizando a programação orientada a objetos seria a melhor escolha. A `class CurrencyConverter` representa os métodos e atributos do conversor. Os atributos relacionados a cotação das moedas deixei privado para evitar problemas.
+Para criação desse projeto identifiquei que utilizando a programação orientada a objetos seria a melhor escolha. A `class CurrencyConverter` carrega os métodos e atributos do conversor.
 
 Para realizar a requisição a API da Awesome API usei o recurso `fetch` do Javascript moderno. A formatação da moeda fiz com o recurso `Intl.NumberFormat` que permite a formatação de acordo com a localização.
 
 **Techs:** HTML, CSS, Javascript
 
-
-## Exemplo de uso
-
-![Exemplo de uso do conversor](./images/gif-usage.gif)
-
-
-## 🚀 Sobre
-Olá eu sou Álvaro Emanuel, desenvolvedor front-end...
-
-Entre em contato: alvarodiasribeiro16@gmail.com
-
-
 ## 🔗 Links
 [![Github](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://github.com/AlvaroEmanuel20/)
 [![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/alvaroemanuel20)
+Entre em contato: alvarodiasribeiro16@gmail.com
 
--- js/index.js
+++ js/index.js
@@ -1,172 +1,138 @@
 class CurrencyConverter {
-  #dolarToBRL;
-  #realToUSD;
-  #euroToBRL;
-  #realToEUR;
-  #dolarToEUR;
-  #euroToUSD;
-
-  constructor() {
-    this.#dolarToBRL = 0;
-    this.#realToUSD = 0;
-    this.#euroToBRL = 0;
-    this.#realToEUR = 0;
-    this.#dolarToEUR = 0;
-    this.#euroToUSD = 0;
-    this.createDateUSD = "";
-    this.createDateEUR = "";
-    this.amount = document.getElementById("amount");
-    this.fromCurrency = document.getElementById("from");
-    this.toCurrency = document.getElementById("to");
-    this.result = document.getElementById("result");
-    
-    //CURRENCY FORMATS
-    this.formatterBR = new Intl.NumberFormat("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-      minimumFractionDigits: 2
-    });
-    this.formatterUS = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    });
-    this.formatterEU = new Intl.NumberFormat("en-GB", {
-      style: "currency",
-      currency: "EUR",
-      minimumFractionDigits: 2
-    });
-  }
-
-  //GETTERS FUNCTIONS
-  getUSDToBRL() {
-    return this.#dolarToBRL;
-  }
-
-  getEURToBRL() {
-    return this.#euroToBRL;
-  }
-
-  //SETTERS FUNCTIONS
-  setUSDToBRL(value) {
-    this.#dolarToBRL = value;
-  }
-
-  setBRLToUSD(value) {
-    this.#realToUSD = value;
-  }
-
-  setEURToBRL(value) {
-    this.#euroToBRL = value;
-  }
-
-  setBRLToEUR(value) {
-    this.#realToEUR = value;
-  }
-
-  setUSDToEUR(value) {
-    this.#dolarToEUR = value;
-  }
-
-  setEURToUSD(value) {
-    this.#euroToUSD = value;
-  }
-
-  currencyOperation() {
-    const from = this.fromCurrency.value;
-    const to = this.toCurrency.value;
-    let calculate = "";
-
-    if (from === "USD" && to === "BRL") {
-      calculate = (this.amount.value * this.#dolarToBRL).toFixed(2);
-    } else if (from === "BRL" && to === "USD") {
-      calculate = (this.amount.value * this.#realToUSD).toFixed(2);
-    } else if (from === "EUR" && to === "BRL") {
-      calculate = (this.amount.value * this.#euroToBRL).toFixed(2);
-    } else if (from === "BRL" && to === "EUR") {
-      calculate = (this.amount.value * this.#realToEUR).toFixed(2);
-    } else if (from === "USD" && to === "EUR") {
-      calculate = (this.amount.value * this.#dolarToEUR).toFixed(2);
-    } else if (from === "EUR" && to === "USD") {
-      calculate = (this.amount.value * this.#euroToUSD).toFixed(2);
-    } else {
-      const errorNotice = document.createElement("p");
-      const divNotice = document.getElementById("errorNotice");
-      errorNotice.style.color = "red";
-      errorNotice.innerHTML = "Selecione moedas diferentes";
-      errorNotice.style.marginTop = "5px";
-      divNotice.appendChild(errorNotice);
-
-      return calculate = false;
+    constructor() {
+        this.usdToBrl;
+        this.brlToUsd;
+        this.eurToBrl;
+        this.brlToEur;
+		this.usdToEur;
+		this.eurToUsd;
+        this.dateUSD;
+        this.dateEUR;
+        this.amount = document.getElementById("amount");
+        this.fromCurrency = document.getElementById("from");
+        this.toCurrency = document.getElementById("to");
+        this.result = document.getElementById("result");
     }
 
-    return calculate;
-  }
+	async getCurrency() {
+		const url = "https://economia.awesomeapi.com.br/last/USD-BRL,BRL-USD,EUR-BRL,BRL-EUR,USD-EUR,EUR-USD";
+		const response = await fetch (url);
+		const data = await response.json();
+	
+		if (response.ok) {
+			this.usdToBrl = data.USDBRL.bid;
+			this.brlToUsd = data.BRLUSD.bid;
+			this.eurToBrl = data.EURBRL.bid;
+			this.brlToEur = data.BRLEUR.bid;
+			this.usdToEur = data.USDEUR.bid;
+			this.eurToUsd = data.EURUSD.bid;
+			this.dateUSD = this.dateFormatter(data.USDBRL.create_date);
+			this.dateEUR = this.dateFormatter(data.EURBRL.create_date);
+		} else {
+			alert("Erro inesperado, por favor volte mais tarde");
+		}
+	}
+
+	currencyFormatter(money, country) {
+		const formatter = new Intl.NumberFormat(country, {
+            style: "currency",
+            currency: money,
+            minimumFractionDigits: 2,
+        });
+
+		return formatter;
+	}
+
+	dateFormatter(dateData) {
+		const date = new Date(dateData);
+		const day = date.getDate() <= 9 ? `0${date.getDate()}` : date.getDate();
+		const month = date.getMonth() <= 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+		const year = date.getFullYear();
+
+		return `${day}/${month}/${year}`;
+	}
+
+    currencyOperation() {
+        const from = this.fromCurrency.value;
+        const to = this.toCurrency.value;
+        let resultValue = "";
+
+        if (from === "USD" && to === "BRL") {
+            resultValue = (this.amount.value * this.usdToBrl).toFixed(2);
+        } else if (from === "BRL" && to === "USD") {
+            resultValue = (this.amount.value * this.brlToUsd).toFixed(2);
+        } else if (from === "EUR" && to === "BRL") {
+            resultValue = (this.amount.value * this.eurToBrl).toFixed(2);
+        } else if (from === "BRL" && to === "EUR") {
+            resultValue = (this.amount.value * this.brlToEur).toFixed(2);
+        } else if (from === "USD" && to === "EUR") {
+            resultValue = (this.amount.value * this.usdToEur).toFixed(2);
+		} else if (from === "EUR" && to === "USD") {
+            resultValue = (this.amount.value * this.eurToUsd).toFixed(2);
+		} else {
+            this.errorMessage();
+            return resultValue = false;
+        }
+
+        return resultValue;
+    }
+
+	errorMessage() {
+		const errorNotice = document.createElement("p");
+		const divNotice = document.getElementById("errorNotice");
+		errorNotice.style.color = "red";
+		errorNotice.innerHTML = "Selecione moedas diferentes";
+		errorNotice.style.marginTop = "5px";
+		divNotice.appendChild(errorNotice);
+
+		setTimeout(() => divNotice.removeChild(errorNotice), 1000);
+	}
+
+	currencyReference() {
+		const dolarReference = document.getElementById("dolar-reference");
+		const euroReference = document.getElementById("euro-reference");
+		
+		const gifLoading = document.createElement("img");
+		gifLoading.src = "./images/gif-loading.gif";
+		gifLoading.style.width = "30px";
+		dolarReference.appendChild(gifLoading);
+	
+		setInterval(() => {
+			dolarReference.innerHTML = `Dólar: ${this.usdToBrl} (Atualizado: ${this.dateUSD})`;
+			euroReference.innerHTML = `Euro: ${this.eurToBrl} (Atualizado: ${this.dateEUR})`;
+		}, 1500);
+	}
 }
 
-const currencyConverter = new CurrencyConverter();
-
-async function getCurrency() {
-  const response = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,BRL-USD,EUR-BRL,BRL-EUR,USD-EUR,EUR-USD");
-  if (response.ok) {
-    return response.json();
-  } else {
-    alert("Erro inesperado, por favor volte mais tarde");
-  }
-}
-
-getCurrency().then(data => {
-  currencyConverter.setUSDToBRL(data.USDBRL.bid);
-  currencyConverter.setBRLToUSD(data.BRLUSD.bid);
-  currencyConverter.setEURToBRL(data.EURBRL.bid);
-  currencyConverter.setBRLToEUR(data.BRLEUR.bid);
-  currencyConverter.setUSDToEUR(data.USDEUR.bid);
-  currencyConverter.setEURToUSD(data.EURUSD.bid);
-  currencyConverter.createDateUSD = data.USDBRL.create_date;
-  currencyConverter.createDateEUR = data.EURBRL.create_date;
-});
+const converter = new CurrencyConverter();
+converter.getCurrency();
+converter.currencyReference();
 
 function convertAmount() {
-  const result = currencyConverter.currencyOperation();
-  const from = currencyConverter.fromCurrency.value;
-  const to = currencyConverter.toCurrency.value;
+	const resultValue = converter.currencyOperation();
 
-  if (currencyConverter.amount.value === "") {
-    currencyConverter.result.innerHTML = "Preencha a quantia";
-  } else if (result === false) {
-    currencyConverter.result.innerHTML = "Preencha novamente";
-  } else {
-    if (to === "USD") {
-      currencyConverter.result.innerHTML = currencyConverter.formatterUS.format(result);
-    } else if (to === "BRL") {
-      currencyConverter.result.innerHTML = currencyConverter.formatterBR.format(result);
-    } else if (to === "EUR") {
-      currencyConverter.result.innerHTML = currencyConverter.formatterEU.format(result);
+    if (!converter.amount.value) {
+        converter.result.innerHTML = "Preencha a quantia";
+    } else if (resultValue === false) {
+        converter.result.innerHTML = "Preencha corretamente";
+    } else {
+        if (converter.toCurrency.value === "USD") {
+            converter.result.innerHTML =
+                converter.currencyFormatter("USD", "en-US").format(resultValue);
+        } else if (converter.toCurrency.value === "BRL") {
+            converter.result.innerHTML =
+                converter.currencyFormatter("BRL", "pt-br").format(resultValue);
+        } else if (converter.toCurrency.value === "EUR") {
+            converter.result.innerHTML =
+                converter.currencyFormatter("EUR", "en-GB").format(resultValue);
+        }
     }
-  }
 
-  currencyConverter.amount.value = "";
-  from.value = "USD";
-  to.value = "BRL";
+    converter.amount.value = "";
 }
 
 function switchCurrency() {
-  let aux = currencyConverter.fromCurrency.value;
-  currencyConverter.fromCurrency.value = currencyConverter.toCurrency.value;
-  currencyConverter.toCurrency.value = aux;
+	const aux = converter.fromCurrency.value;
+    converter.fromCurrency.value = converter.toCurrency.value;
+    converter.toCurrency.value = aux;
 }
-
-function currencyReference() {
-  const dolarReference = document.getElementById("dolar-reference");
-  const euroReference = document.getElementById("euro-reference");
-  const gifLoading = document.createElement("img");
-  gifLoading.src = "./images/gif-loading.gif";
-  gifLoading.style.width = "30px";
-  dolarReference.appendChild(gifLoading);
-  setInterval(() => {
-    dolarReference.innerHTML = `Dólar: ${currencyConverter.getUSDToBRL()} (Atualizado: ${currencyConverter.createDateUSD})`;
-    euroReference.innerHTML = `Euro: ${currencyConverter.getEURToBRL()} (Atualizado: ${currencyConverter.createDateEUR})`;
-  }, 2000);
-}
-
-currencyReference();
